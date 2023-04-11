144579744f6102e8fc384b1608f93119ff976737
soorin1990 <124007834+soorin1990@users.noreply.github.com>
2023-04-11 16:32:52 -0500

    removed unused variables from edit script

--- Runtime/Internal/DrawIfAttribute.cs
+++ Runtime/Internal/DrawIfAttribute.cs
@@ -12,16 +12,6 @@ public class DrawIfAttribute : PropertyAttribute
     public string comparedPropertyName { get; private set; }
     public object comparedValue { get; private set; }
     public bool invert { get; private set; }
-    public DisablingType disablingType { get; private set; }
- 
-    /// <summary>
-    /// Types of comperisons.
-    /// </summary>
-    public enum DisablingType
-    {
-        ReadOnly = 2,
-        DontDraw = 3
-    }
  
     #endregion
  
@@ -30,12 +20,11 @@ public class DrawIfAttribute : PropertyAttribute
     /// </summary>
     /// <param name="comparedPropertyName">The name of the property that is being compared (case sensitive).</param>
     /// <param name="comparedValue">The value the property is being compared to.</param>
-    /// <param name="disablingType">The type of disabling that should happen if the condition is NOT met. Defaulted to DisablingType.DontDraw.</param>
+    /// <param name="reverse">applies reverse of the condition result i.e hides if the condition is true / shows if the condition is false.</param>
     public DrawIfAttribute(string comparedPropertyName, object comparedValue, bool reverse = false)
     {
         this.comparedPropertyName = comparedPropertyName;
         this.comparedValue = comparedValue;
-        this.disablingType = disablingType;
         invert = (bool) reverse;
     }
 }
